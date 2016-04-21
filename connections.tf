26e7aac23daaea5c7f3dd138fa75c52cc403aeb1
Adron Hall <adronhall@gmail.com>
2016-04-21 15:25:54 -0700

    Cleaned up project more and removed account.json from project path.

--- connections.tf
+++ connections.tf
@@ -1,17 +1,6 @@
-variable "default_region" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
-# Configure the AWS Provider
-provider "aws" {
-   access_key = "${var.aws_access_key}"
-   secret_key = "${var.aws_secret_key}"
-   region = "${var.default_region}"
-}
-
 # Configure the Google Cloud provider
 provider "google" {
-  credentials = "${file("account.json")}"
+  credentials = "${file("../secrets/account.json")}"
   project     = "that-big-universe"
   region      = "us-central1"
 }
