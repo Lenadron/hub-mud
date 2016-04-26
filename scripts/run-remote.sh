31d44e41b818b33f6cd128147545f378d78f5a64
Adron Hall <adronhall@gmail.com>
2016-04-26 10:40:47 -0700

    Additional renames, etc.

--- scripts/run-remote.sh
+++ scripts/run-remote.sh
@@ -22,3 +22,6 @@ sudo unzip terraform_0.6.14_linux_amd64.zip
 cd ../packer_0_10_0
 sudo curl -O https://releases.hashicorp.com/packer/0.10.0/packer_0.10.0_linux_amd64.zip
 sudo unzip packer_0.10.0_linux_amd64.zip
+
+cd ~
+mkdir workspace
